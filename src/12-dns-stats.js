907d96a0709961d919253597f41cd175dc2b5a19
marynachubrei <marinaciubrei@gmail.com>
2021-04-12 23:17:04 +0300

    feat: add 12th task

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,27 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const b = domains.map((elem) => `.${elem.split('.').reverse().join('.')}`);
+  const ar = [];
+  for (let i = 0; i < b.length; i++) {
+    if (b[i].match(/^\.[a-z]+/)) {
+      ar.push(b[i].match(/^\.[a-z]+/)[0]);
+    }
+    if (b[i].match(/^\.[a-z]+\.[a-z]+/)) {
+      ar.push(b[i].match(/^\.[a-z]+\.[a-z]+/)[0]);
+    }
+    if (b[i].match(/^\.[a-z]+\.[a-z]+\.[a-z]+/)) {
+      ar.push(b[i].match(/^\.[a-z]+\.[a-z]+\.[a-z]+/)[0]);
+    }
+  }
+  const obj = ar.map((name) => ({
+    count: 1, name: `${name}`,
+  })).reduce((a, c) => {
+    const m = a;
+    m[c.name] = (m[c.name] || 0) + c.count; return m;
+  }, {});
+  return obj;
 }
 
 module.exports = getDNSStats;
